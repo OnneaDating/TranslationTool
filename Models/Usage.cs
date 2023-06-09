8cedb087b55e02620a63067d23c96d95d916cc11
Jim Kangosjarvi <jim.kangosjarvi@skf.com>
2023-06-09 19:18:18 +0300

    Init

--- Models/Usage.cs
+++ Models/Usage.cs
@@ -3,5 +3,5 @@ public class Usage
 {
     public int prompt_tokens { get; set; }
     public int completion_tokens { get; set; }
-    public int total_tokens { get; set; }
+    public int total_tokens { get; set; } 
 }
